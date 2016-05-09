63b7ff177b67f309c575b52531c42a2e457335bd
Thomas M. DuBuisson <tommd@galois.com>
2016-05-09 10:45:57 -0700

    Remove the now-unused Test/MD5 file - inlined into Test/main.

--- Test/MD5.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE ExistentialQuantification #-}
-module Test.MD5 where
-
-import Test.QuickCheck
-import Test.Crypto
-import Data.Digest.Pure.MD5
-
-test = runTests (makeMD5Tests (undefined :: MD5Digest))
